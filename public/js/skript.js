a2c458bf4a90891861ab43cbf1fe72ede8ef3cef
der20077 <danilvedenapin2@gmail.com>
2026-03-04 17:08:29 +0300

    Add files via upload

---
+++ public/js/skript.js
@@ -0,0 +1,67 @@
+document.addEventListener("DOMContentLoaded", loadToDo);
+
+function loadToDo() {
+  const input = document.getElementById("inputTask");
+  const btnTask = document.getElementById("addTaskBtn");
+  const listTask = document.getElementById("list");
+
+  const loadTask = () => {
+    const task = JSON.parse(localStorage.getItem("tasks")) || [];
+    return task;
+  };
+
+  const saveTask = (tasks) => {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  };
+
+  const tasks = loadTask();
+  const addtask = () => {
+    const taskHtml = tasks
+      .map((task) => {
+        return `<li>
+        <input type="checkbox" ${task.compleadet ? "checked" : " "}>
+        <span>${task.text}</span>
+        <button class="btnDel">x</button>
+      </li>`;
+      })
+      .join("");
+
+    listTask.innerHTML = taskHtml;
+
+    listTask.querySelectorAll(".btnDel").forEach((btn, index) => {
+      btn.addEventListener("click", () => {
+        tasks.splice(index, 1);
+        saveTask(tasks);
+        addtask();
+      });
+    });
+    listTask
+      .querySelectorAll('input[type="checkbox"]')
+      .forEach((checbox, index) => {
+        checbox.addEventListener("change", (event) => {
+          event.preventDefault();
+          tasks[index].compleadet = event.target.checked;
+          saveTask(tasks);
+          addtask();
+        });
+      });
+  };
+
+  btnTask.addEventListener("click", (event) => {
+    event.preventDefault();
+    const textTask = input.value.trim();
+
+    if (textTask) {
+      const newTask = {
+        id: Date.now(),
+        text: textTask,
+        compleadet: false,
+      };
+      tasks.push(newTask);
+      saveTask(tasks);
+      input.value = "";
+    }
+    addtask();
+  });
+  addtask();
+}
